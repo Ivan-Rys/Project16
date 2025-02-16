ac8944044e020a9ab5b5e4ef0703a142e090cd6a
Anna <anutalisovec@gmail.com>
2025-02-16 16:49:29 +0300

    add check fanction

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
+bool check(string a, string b) {
+	return a == b;
+}
+
 int main() {
+	string a, b;
 
+	cout << "Input your number: " << endl;
+	cin >> a >> b;
 
+	string msg = check("abc", "abc") ? "Yes" : "No";
 
+	cout << a << endl;
+	cout << b << endl;
+	cout << msg << endl;
 
 	return 0;
 }
